590dcc56de197e53d8c320112808e3ff66101818
Jack Wartell <jack.wartell.20@cnu.edu>
2022-03-01 18:43:40 -0500

    Start reading in CSV file

--- src/main.py
+++ src/main.py
@@ -1,3 +1,7 @@
+import csv
+from enum import Enum
+
+
 # This is a sample Python script.
 
 # Press Shift+F10 to execute it or replace it with your code.
@@ -11,6 +15,11 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
+    food_list = []
+    with open("resources/FoodInputData.csv") as fin:
+        reader = csv.reader(fin)
+        for i in reader:
+            for j in i:
+                food_list.append(j)
+    print(food_list)
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
