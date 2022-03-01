7edf63c9456a2dcdfe2ac17da7c6098d9fad1823
Jack Wartell <jack.wartell.20@cnu.edu>
2022-03-01 18:47:13 -0500

    Add types of food to list

--- src/main.py
+++ src/main.py
@@ -6,11 +6,8 @@ from enum import Enum
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def clean_list(input_list):
+    return list(dict.fromkeys(input_list))
 
 
 # Press the green button in the gutter to run the script.
@@ -20,6 +17,7 @@ if __name__ == '__main__':
         reader = csv.reader(fin)
         for i in reader:
             for j in i:
-                food_list.append(j)
+                food_list.append(j.split(";")[0])
+    food_list = clean_list(food_list)
     print(food_list)
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
