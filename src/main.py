0337454a3ceab70b04f59b6ae64cb83c1b9154c8
Jack Wartell <jack.wartell.20@cnu.edu>
2022-03-02 23:07:50 -0500

    Define function stubs and remove placeholder value from list & enum

--- src/main.py
+++ src/main.py
@@ -1,10 +1,25 @@
 import csv
 from enum import Enum
+
+
 # from matplotlib import pyplot as plt
 
 
 def clean_list(input_list):
-    return list(dict.fromkeys(input_list))
+    # removes duplicates (and first val) from list
+    return list(dict.fromkeys(input_list[1:]))
+
+
+def sort_by_cals():
+    pass
+
+
+def sort_by_fat():
+    pass
+
+
+def sort_by_taste():
+    pass
 
 
 # Press the green button in the gutter to run the script.
@@ -21,5 +36,3 @@ if __name__ == '__main__':
     # Instead of creating enum directly from list,
     # create a dict from the elements with the value being the index
     Food = Enum('Food', {name: index for index, name in enumerate(food_list)})
-
-    
